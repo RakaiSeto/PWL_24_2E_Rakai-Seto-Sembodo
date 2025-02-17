fe57cbdbfbfb77bf374292a47b36e1bdb5661abf
Rakai Seto <mamangrakai@gmail.com>
2025-02-17 19:36:24 +0700

    Controller

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,21 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return 'Hello World!';
-});
+Route::get('/', [HomeController::class,'index']);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
 
 Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang!';
-});
-
-Route::get('/about', function () {
-    return "Rakai Seto Sembodo \n 2341720135";
-});
+Route::get('/about', [AboutController::class, 'index']);
 
 Route::get("/user/{name?}", function ($name = 'john') {
     return 'Nama saya ' . $name;
@@ -27,10 +26,11 @@ Route::get('/posts/{post}/comments/{comment}', function
  return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman artikel dengan ID '.$id;
-});
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
 
 Route::get('/user/profile', function() {
     //
 })->name('profile');
+
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(methods: ['create', 'store', 'destroy', 'update', 'edit']);
