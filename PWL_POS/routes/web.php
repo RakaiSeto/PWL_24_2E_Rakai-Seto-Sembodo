221f1dc940afedbcca0936ddc1579e28b9aa2b28
Rakai Seto <mamangrakai@gmail.com>
2025-03-02 20:51:28 +0700

    Jobsheet3_6

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Monolog\Level;
 
@@ -11,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class,'index']);
