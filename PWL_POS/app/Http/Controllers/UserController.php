38b49e90305352687a2ed72325056c2febc01e81
Rakai Seto <mamangrakai@gmail.com>
2025-03-06 13:05:53 +0700

    Jobsheet4_2.4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,16 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'level_id' => 2,
-        //     'username'=> 'manager_tiga',
-        //     'name' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
-
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'name' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
